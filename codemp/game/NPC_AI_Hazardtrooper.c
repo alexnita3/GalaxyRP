0b291997e2228c461f75f34522b66cc760927455
eezstreet <eezstreet@live.com>
2013-04-26 13:49:51 -0400

    Added NPC_AI_Hazardtrooper.c file, currently incomplete. TODO: rewrite from scratch

---
+++ codemp/game/NPC_AI_Hazardtrooper.c
@@ -0,0 +1,38 @@
+/*
+This file is part of OpenJK.
+
+    OpenJK is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    OpenJK is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenJK.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Copyright 2001-2013 Raven Software
+
+////////////////////////////////////////////////////////////////////////////////////////
+// RAVEN SOFTWARE - STAR WARS: JK II
+//  (c) 2002 Activision
+//
+// Troopers
+//
+// TODO
+// ----
+//
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+#include "b_local.h"
+#include "g_nav.h"
+#include "anims.h"
+
