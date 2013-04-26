3a2d755880a8214e92147844850d6c470202ee8e
eezstreet <eezstreet@live.com>
2013-04-26 14:18:40 -0400

    Some more work on Hazardtrooper AI (warning: still not done)

--- codemp/game/NPC_AI_Hazardtrooper.c
+++ codemp/game/NPC_AI_Hazardtrooper.c
@@ -36,3 +36,119 @@ This file is part of OpenJK.
 #include "g_nav.h"
 #include "anims.h"
 
+enum
+{
+	SPEECH_CHASE,
+	SPEECH_CONFUSED,
+	SPEECH_COVER,
+	SPEECH_DETECTED,
+	SPEECH_GIVEUP,
+	SPEECH_LOOK,
+	SPEECH_LOST,
+	SPEECH_OUTFLANK,
+	SPEECH_ESCAPING,
+	SPEECH_SIGHT,
+	SPEECH_SOUND,
+	SPEECH_SUSPICIOUS,
+	SPEECH_YELL,
+	SPEECH_PUSHED
+};
+
+extern void G_AddVoiceEvent( gentity_t *self, int event, int speakDebounceTime );
+static void HT_Speech( gentity_t *self, int speechType, float failChance )
+{
+	if ( random() < failChance )
+	{
+		return;
+	}
+
+	if ( failChance >= 0 )
+	{//a negative failChance makes it always talk
+		if ( self->NPC->group )
+		{//group AI speech debounce timer
+			if ( self->NPC->group->speechDebounceTime > level.time )
+			{
+				return;
+			}
+			/*
+			else if ( !self->NPC->group->enemy )
+			{
+				if ( groupSpeechDebounceTime[self->client->playerTeam] > level.time )
+				{
+					return;
+				}
+			}
+			*/
+		}
+		else if ( !TIMER_Done( self, "chatter" ) )
+		{//personal timer
+			return;
+		}
+	}
+
+	TIMER_Set( self, "chatter", Q_irand( 2000, 4000 ) );
+
+	if ( self->NPC->blockedSpeechDebounceTime > level.time )
+	{
+		return;
+	}
+
+	switch( speechType )
+	{
+	case SPEECH_CHASE:
+		G_AddVoiceEvent( self, Q_irand(EV_CHASE1, EV_CHASE3), 2000 );
+		break;
+	case SPEECH_CONFUSED:
+		G_AddVoiceEvent( self, Q_irand(EV_CONFUSE1, EV_CONFUSE3), 2000 );
+		break;
+	case SPEECH_COVER:
+		G_AddVoiceEvent( self, Q_irand(EV_COVER1, EV_COVER5), 2000 );
+		break;
+	case SPEECH_DETECTED:
+		G_AddVoiceEvent( self, Q_irand(EV_DETECTED1, EV_DETECTED5), 2000 );
+		break;
+	case SPEECH_GIVEUP:
+		G_AddVoiceEvent( self, Q_irand(EV_GIVEUP1, EV_GIVEUP4), 2000 );
+		break;
+	case SPEECH_LOOK:
+		G_AddVoiceEvent( self, Q_irand(EV_LOOK1, EV_LOOK2), 2000 );
+		break;
+	case SPEECH_LOST:
+		G_AddVoiceEvent( self, EV_LOST1, 2000 );
+		break;
+	case SPEECH_OUTFLANK:
+		G_AddVoiceEvent( self, Q_irand(EV_OUTFLANK1, EV_OUTFLANK2), 2000 );
+		break;
+	case SPEECH_ESCAPING:
+		G_AddVoiceEvent( self, Q_irand(EV_ESCAPING1, EV_ESCAPING3), 2000 );
+		break;
+	case SPEECH_SIGHT:
+		G_AddVoiceEvent( self, Q_irand(EV_SIGHT1, EV_SIGHT3), 2000 );
+		break;
+	case SPEECH_SOUND:
+		G_AddVoiceEvent( self, Q_irand(EV_SOUND1, EV_SOUND3), 2000 );
+		break;
+	case SPEECH_SUSPICIOUS:
+		G_AddVoiceEvent( self, Q_irand(EV_SUSPICIOUS1, EV_SUSPICIOUS5), 2000 );
+		break;
+	case SPEECH_YELL:
+		G_AddVoiceEvent( self, Q_irand( EV_ANGER1, EV_ANGER3 ), 2000 );
+		break;
+	case SPEECH_PUSHED:
+		G_AddVoiceEvent( self, Q_irand( EV_PUSHED1, EV_PUSHED3 ), 2000 );
+		break;
+	default:
+		break;
+	}
+
+	self->NPC->blockedSpeechDebounceTime = level.time + 2000;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+// The Troop
+//
+// Troopers primarly derive their behavior from cooperation as a collective group of
+// individuals.  They join Troops, each of which has a leader responsible for direcing
+// the movement of the rest of the group.
+//
+////////////////////////////////////////////////////////////////////////////////////////
