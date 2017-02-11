67ca7d05679f312a8e86352bf39abd36b106b7dd
Zyk <zyk6870@gmail.com>
2017-02-11 15:50:13 -0200

    Changed version to 3.53. Added NPC_type field to /entedit.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -44,7 +44,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"Zyk OpenJK Mod v3.52"
+#define	GAMEVERSION	"Zyk OpenJK Mod v3.53"
 
 #define SECURITY_LOG "security.log"
 
