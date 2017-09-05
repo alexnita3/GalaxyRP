39a8af448565b45e69efc7cb8d5f69c7361224a7
Zyk <zyk6870@gmail.com>
2017-09-05 15:32:38 -0300

    Changed the way the max mp is calculated for Magic Master class.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -3787,9 +3787,12 @@ int zyk_max_magic_power(gentity_t *ent)
 	int max_mp = ent->client->pers.level * 3;
 
 	if (ent->client->pers.rpg_class == 8) // zyk: Magic Master has more Magic Power
-		return (max_mp + (75 * (ent->client->pers.skill_levels[55] + 1)));
-	else
-		return max_mp;
+	{
+		max_mp = ent->client->pers.level * 4;
+		return (max_mp + (50 * (ent->client->pers.skill_levels[55] + 1)));
+	}
+
+	return max_mp;
 }
 
 extern void poison_mushrooms(gentity_t *ent, int min_distance, int max_distance);
@@ -9797,7 +9800,7 @@ void Cmd_ListAccount_f( gentity_t *ent ) {
 					if (i == 55)
 						trap->SendServerCommand( ent-g_entities, va("print \"^3Force Power: ^7increases the max force power you have. Necessary to allow you to use force powers and force-based skills\n\"") );
 					if (i == 56)
-						trap->SendServerCommand( ent-g_entities, va("print \"^3Improvements:\n^7Free Warrior gets more damage and more resistance to damage\nForce User gets more saber damage and force regens faster\nBounty Hunter gets more gun damage, max ammo, credits in battle, jetpack fuel, sentry gun health, and E-Web health\nArmored Soldier gets more resistance to damage\nMonk gets more run speed, melee damage and melee attack speed\nStealth Attacker gets more gun damage and more resistance to electric attacks\nDuelist gets more saber and melee damage and faster force regen\nForce Gunner gets more damage and more resistance to damage\nMagic Master gets more melee damage, spends less jetpack fuel, gets more Magic Power, gives new magic bolt types, gives new magic powers and has less special power cooldown\nForce Tank gets more resistance to damage\n\"") );
+						trap->SendServerCommand( ent-g_entities, va("print \"^3Improvements:\n^7Free Warrior gets more damage and more resistance to damage\nForce User gets more saber damage and force regens faster\nBounty Hunter gets more gun damage, max ammo, credits in battle, jetpack fuel, sentry gun health, and E-Web health\nArmored Soldier gets more resistance to damage\nMonk gets more run speed, melee damage and melee attack speed\nStealth Attacker gets more gun damage and more resistance to electric attacks\nDuelist gets more saber and melee damage and faster force regen\nForce Gunner gets more damage and more resistance to damage\nMagic Master gets more melee damage, spends less jetpack fuel, gets more Magic Points, gives new magic bolt types, gives new magic powers and has less special power cooldown\nForce Tank gets more resistance to damage\n\"") );
 				}
 				else if (Q_stricmp( arg1, "l" ) == 0)
 				{
