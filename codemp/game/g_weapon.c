4ba7ad8375daf3a24e8a75f73af3d425f4c0933f
Zyk <zyk6870@gmail.com>
2018-09-30 21:02:29 -0300

    Fixed bug in condition for enemy to deflect gun shots.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_weapon.c
+++ codemp/game/g_weapon.c
@@ -670,7 +670,7 @@ static void WP_DisruptorMainFire( gentity_t *ent )
 
 	if ( render_impact )
 	{
-		if ( tr.entityNum < ENTITYNUM_WORLD && traceEnt->takedamage && zyk_can_deflect_shots(traceEnt))
+		if ( tr.entityNum < ENTITYNUM_WORLD && traceEnt->takedamage && zyk_can_deflect_shots(traceEnt) == qfalse)
 		{
 			if ( traceEnt->client && LogAccuracyHit( traceEnt, ent ))
 			{
