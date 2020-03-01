2e4e439e0fcabf61b0bf38378c642b949fbec6a7
Zyk <zyk6870@gmail.com>
2020-03-01 05:47:53 -0300

    Version 3.75
    Some classes will no longer be able to buy Ammo All, because they
    are not gun classes.
    
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -10649,25 +10649,25 @@ void Cmd_Buy_f( gentity_t *ent ) {
 
 	// zyk: class validations. Some items require certain conditions to be bought
 	if (ent->client->pers.rpg_class == 1 && ((value >= 1 && value <= 7) || (value >= 10 && value <= 13) || 
-		(value >= 17 && value <= 24) || (value >= 34 && value <= 38) || (value >= 41 && value <= 42)))
+		(value >= 17 && value <= 24) || (value >= 34 && value <= 38) || (value >= 41 && value <= 42) || value == 48))
 	{
 		trap->SendServerCommand( ent-g_entities, "print \"Force User can't buy this item.\n\"" );
 		return;
 	}
 	else if (ent->client->pers.rpg_class == 4 && ((value >= 1 && value <= 7) || (value >= 10 && value <= 13) || 
-			(value >= 17 && value <= 24) || (value >= 34 && value <= 38) || (value >= 41 && value <= 42)))
+			(value >= 17 && value <= 24) || (value >= 34 && value <= 38) || (value >= 41 && value <= 42) || value == 48))
 	{
 		trap->SendServerCommand( ent-g_entities, "print \"Monk can't buy this item.\n\"" );
 		return;
 	}
 	else if (ent->client->pers.rpg_class == 6 && ((value >= 1 && value <= 7) || (value >= 10 && value <= 13) || 
-			(value >= 17 && value <= 24) || (value >= 34 && value <= 38) || (value >= 41 && value <= 42)))
+			(value >= 17 && value <= 24) || (value >= 34 && value <= 38) || (value >= 41 && value <= 42) || value == 48))
 	{
 		trap->SendServerCommand( ent-g_entities, "print \"Duelist can't buy this item.\n\"" );
 		return;
 	}
 	else if (ent->client->pers.rpg_class == 8 && ((value >= 1 && value <= 7) || (value >= 10 && value <= 13) || 
-			(value >= 17 && value <= 24) || (value >= 35 && value <= 38)))
+			(value >= 17 && value <= 24) || (value >= 35 && value <= 38) || value == 48))
 	{
 		trap->SendServerCommand( ent-g_entities, "print \"Magic Master can't buy this item.\n\"" );
 		return;
