9a724649b6a3f6a3f763e256c81e203982a94910
Alex Nita <67995057+alexnita3@users.noreply.github.com>
2020-10-19 20:55:10 +0100

    Revert "Merge pull request #8 from alexnita3/feature/change-mod-version"
    This reverts commit c01880644ac91db3928b911daeba7f2b1f1a0416, reversing
    changes made to e69be060c171d88725c4a2ed76e99dfad6fdb077.

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -37,7 +37,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"Galaxy RP Mod v0.2"
+#define	GAMEVERSION	"Zyk OpenJK Mod"
 
 #define BODY_QUEUE_SIZE		8
 
--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -2053,7 +2053,7 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 		{
 			//add paranthesis for OOC chat (I know it's a workaround and it should be done better but it works)
 			char beginning[] = "((";
-			char end[] = "^1))";
+			char end[] = "))";
 
 			strcat(ooc_text, beginning);
 			strcat(ooc_text, text);
