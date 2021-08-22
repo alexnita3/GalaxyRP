f892a59a8595223c0ca3415932532e380a452e0f
Alex Nita <67995057+alexnita3@users.noreply.github.com>
2021-08-22 17:27:01 +0100

    Fixed small bug with the salute emote.

--- codemp/game/bg_pmove.c
+++ codemp/game/bg_pmove.c
@@ -11074,9 +11074,6 @@ void PmoveSingle (pmove_t *pmove) {
 		// keep the talk button set tho for when the cmd.serverTime > 66 msec
 		// and the same cmd is used multiple times in Pmove
 		pmove->cmd.buttons = BUTTON_TALK;
-		pmove->cmd.forwardmove = 0;
-		pmove->cmd.rightmove = 0;
-		pmove->cmd.upmove = 0;
 	}
 
 	// clear all pmove local vars
