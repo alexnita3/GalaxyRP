bc93fce4be1951598efc602fbc2cacd1007df9cf
TriForceX <triforce@gznetwork.com>
2021-10-13 02:39:01 -0300

    Force file path for generated server files

--- assets/server/GalaxyRP-Host.bat
+++ assets/server/GalaxyRP-Host.bat
@@ -5,6 +5,7 @@ set jk_executable=openjkded.x86.exe
 set jk_dedicated=2
 set jk_net_port=29070
 set jk_config=server.cfg
+set jk_force_path=true
 
 :: Executable check
 if not exist "%cd%\..\%jk_executable%" (
@@ -17,4 +18,8 @@ if not exist "%cd%\..\%jk_executable%" (
 
 :: Launch options
 cd..
-%jk_executable% +set dedicated %jk_dedicated% +set net_port %jk_net_port% +set fs_game GalaxyRP +exec %jk_config%
+if "%jk_force_path%" == "true" (
+	%jk_executable% +set dedicated %jk_dedicated% +set net_port %jk_net_port% +set fs_game GalaxyRP +set fs_homepath %cd% +exec %jk_config%
+) else (
+	%jk_executable% +set dedicated %jk_dedicated% +set net_port %jk_net_port% +set fs_game GalaxyRP +exec %jk_config%
+)
